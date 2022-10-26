a29c0e30f6ebb4ce11db92166ca2fff26498588d
Gabe Mancino <gmancino.777@gmail.com>
2022-10-26 09:17:02 -0400

    Change sampling procedure

--- ReadMe.md
+++ ReadMe.md
@@ -14,12 +14,12 @@ Experiments are performed on a Quadro RTX 5000.
 
 Here we discuss several training/implementation differences from the original FastGCN paper:
 
-1. **Sampling neighborhood**: it is well known that FastGCN can suffer from choosing nodes that are disconnected at each layer (see [2] below). Hence, we implement a version of FastGCN which chooses nodes at each layer from the set of 1-hop neighbors (including the current layer) of the current layer; such a scheme is similar to the LADIES algorithm of [2], but we do not re-compute the adjacency matrix (see line 5 in Algorithm 1 of [2]). Additionally, we sample _without_ replacement in this implementation.
+1. **Sampling neighborhood**: it is well known that FastGCN can suffer from choosing nodes that are disconnected at each layer (see [2] below). Hence, we implement a version of FastGCN which chooses nodes at each layer from the set of 1-hop neighbors of the current layer and union these nodes with the entire current layer; such a scheme is similar to the LADIES algorithm of [2], but we do not re-compute the adjacency matrix (see line 5 in Algorithm 1 of [2]). Additionally, we sample _without_ replacement in this implementation.
 2. **Pre-computation**: as remarked in [1], the initial layer of the GCN architecture is fixed relative to the gradient computations; hence we do _not_ perform sampling on the first layer of the GCN, but instead use all neighbor information at this layer (such a scheme is equivalent to pre-computing the initial layer).
 3. **Probability scaling**: equation (10) in [1] indicates the each node's update needs to be appropriately scaled when performing a forward pass of the network; since nodes are chosen from the 1-hop neighbors in this implementations (see 1. above), this distribution is no longer the _global_ distribution defined over all of the nodes, but rather the _local_ distribution defined over the set of 1-hop neighbor nodes. Such a subtlety deviates from the mathematical formulation of FastGCN, but results in _significant_ improvement in the area of testing accuracy.
 4. **Importance sampling**: we implement importance sampling by using the (squared) 2-norm of each column in the adjacency matrix.
 5. **Dataset split**: we follow the practice of [1] in terms of dataset training/validation/testing split: all nodes that do not belong to the testing or validation set are used as training nodes.
-6. **Batch/sample size**: all of our tests use a standard 2-layer GCN architecture; for all datasets, we compute the loss over a mini-batch of 256 training nodes and sample 400 nodes at the next layer (except for Reddit, which we sample 1024 nodes at the next layer). Notice there is no sampling after this (even though this is a 2-layer GCN) as we use pre-computation on the input layer.
+6. **Batch/sample size**: all of our tests use a standard 2-layer GCN architecture; for all datasets, we compute the loss over a mini-batch of 256 training nodes and sample 400 nodes at the next layer (except for Reddit, which uses a mini-batch of 1024 and samples 5120 nodes at the next layer). Notice there is no sampling after this (even though this is a 2-layer GCN) as we use pre-computation on the input layer.
 7. **Other hyperparameters**: we train all models for 200 iterations with the Adam optimizer using a learning rate of 0.01 and employ early stopping if the validation loss does not decrease for 10 iterations (except for Reddit, which is set to 50 iterations). Weight decay is applied (l_2 regularization), row features are normalized only for PubMed, and no dropout is applied. For Cora, PubMed, and CiteSeer the hidden dimension is 16. For Reddit, the hidden dimension is 128.
 8. **Inference**: we perform inference using all of the nodes in the network (i.e. a forward pass of the un-batched GCN model is used to perform inference); this is done for both computing the validation loss and computing the testing accuracy.
 
@@ -43,12 +43,12 @@ TRAINING INFORMATION:
 [BATCH] batch size: 256
 [SAMP] layer sample size: 400
 
-[STOP] early stopping at iteration: 76
+[STOP] early stopping at iteration: 57
 
 RESULTS:
-[LOSS] minimum loss: 0.3562300503253937
-[ACC] maximum micro F1 testing accuracy: 87.6 %
-[TIME] 0.924 seconds
+[LOSS] minimum loss: 0.13632288575172424
+[ACC] maximum micro F1 testing accuracy: 87.4 %
+[TIME] 0.8241 seconds
 ========================== ENDING TRAINING ==========================
 ```
 
@@ -61,10 +61,10 @@ We compare the current approach with the original GCN and report the _maximum te
 
 | Dataset | FastGCN | GCN |
 | --- | --- | --- |
-| Cora | 87.3% (0.8 s) | 87.2% (0.56 s) |
-| CiteSeer | 78.9% (0.59 s) | 78.7% (0.47 s) |
-| PubMed | 88.0% (1.5 s) | 88.5% (1.4 s) |
-| Reddit | 93.7% (21.8 s) | 94.8% (330.5 s) |
+| Cora | 87.4% (0.82 s) | 87.2% (0.56 s) |
+| CiteSeer | 78.8% (0.62 s) | 78.7% (0.47 s) |
+| PubMed | 88.3% (1.97 s) | 88.5% (1.4 s) |
+| Reddit | 94.47% (63.2 s) | 94.8% (330.5 s) |
 
 ## References
 
--- models/fastgcn_model.py
+++ models/fastgcn_model.py
@@ -122,7 +122,7 @@ class FastGCN(nn.Module):
                                         replace=False)
 
             # Then compute the subgraphs
-            batch_adjs = self.get_subgraphs_unioned_sampling(init_batch=init_batch,
+            batch_adjs = self.get_subgraphs_concated_sampling(init_batch=init_batch,
                                                      batch_sizes=batch_sizes[1:],
                                                      csr_adj_mat=csr_mat)
 
@@ -254,6 +254,114 @@ class FastGCN(nn.Module):
 
             # Get the next layer of nodes
             new_nodes = np.unique(csr_adj_mat[batch[i], :].nonzero()[1])
+
+        # Initial layer of the adjacency matrix involves pre-computation,
+        # but we already store this, so we only need to slice!
+        adj_out_mats.append(batch[-1])
+
+        return adj_out_mats[::-1]
+
+    @torch.no_grad()
+    def get_subgraphs_concated_sampling(self, init_batch: typing.List[int], batch_sizes: typing.List[int],
+                                       csr_adj_mat: sp.csr_matrix) -> list:
+        """Here, we sample from the set of 1-hop neighbors and always include the base nodes"""
+
+        # Create a dummy version of the batch_sizes
+        batch_sizes.insert(0, 0)
+
+        # Get the initial batch
+        batch = [init_batch]
+        adj_out_mats = []
+
+        # Get the next layer of nodes
+        all_next_nodes = np.unique(csr_adj_mat[batch[0], :].nonzero()[1])
+
+        # Get only the subset of nodes that are 1-hop away
+        new_nodes = np.setdiff1d(all_next_nodes, batch[0])
+        old_nodes = np.setdiff1d(all_next_nodes, new_nodes)
+
+        # Loop over the remaining batches
+        for i in range(1, len(batch_sizes)):
+
+            # Save denominator to avoid re-computing
+            denom = sum(self.samp_probs[new_nodes])
+
+            # Save the probs
+            probs = self.samp_probs[new_nodes] / denom
+
+            # Get a batch
+            sampled = np.random.choice(new_nodes, size=min(batch_sizes[i], len(probs)),
+                                          replace=False, p=probs)
+
+            batch.append(np.concatenate((sampled, old_nodes)))
+
+            # Create the probability distribution over these nodes
+            subgraph_probs = np.concatenate((self.samp_probs[sampled] * len(sampled) / denom, np.ones(old_nodes.shape)))
+
+            # Save adjmats
+            adj_out_mats.append(csr_to_torch_coo(
+                csr_adj_mat[batch[i - 1], :][:, batch[i]].multiply(1. / subgraph_probs)).to(
+                self.device))
+
+            # Get the next layer of nodes
+            all_next_nodes = np.unique(csr_adj_mat[sampled, :].nonzero()[1])
+
+            # Get only the subset of nodes that are 1-hop away
+            new_nodes = np.setdiff1d(all_next_nodes, sampled)
+            old_nodes = np.setdiff1d(all_next_nodes, new_nodes)
+
+        # Initial layer of the adjacency matrix involves pre-computation,
+        # but we already store this, so we only need to slice!
+        adj_out_mats.append(batch[-1])
+
+        return adj_out_mats[::-1]
+
+    @torch.no_grad()
+    def get_subgraphs_union_init_batch_sampling(self, init_batch: typing.List[int], batch_sizes: typing.List[int],
+                                        csr_adj_mat: sp.csr_matrix) -> list:
+        """Here, we sample from the set of 1-hop neighbors and union that with the original training nodes"""
+
+        # Create a dummy version of the batch_sizes
+        batch_sizes.insert(0, 0)
+
+        # Get the initial batch
+        batch = [init_batch]
+        adj_out_mats = []
+
+        # Get the next layer of nodes
+        all_next_nodes = np.unique(csr_adj_mat[batch[0], :].nonzero()[1])
+
+        # Get only the subset of nodes that are 1-hop away
+        new_nodes = np.setdiff1d(all_next_nodes, batch[0])
+
+        # Loop over the remaining batches
+        for i in range(1, len(batch_sizes)):
+
+            # Save denominator to avoid re-computing
+            denom = sum(self.samp_probs[new_nodes])
+
+            # Save the probs
+            probs = self.samp_probs[new_nodes] / denom
+
+            # Get a batch
+            sampled = np.random.choice(new_nodes, size=min(batch_sizes[i], len(probs)),
+                                       replace=False, p=probs)
+
+            batch.append(np.concatenate((sampled, batch[0])))
+
+            # Create the probability distribution over these nodes
+            subgraph_probs = np.concatenate((self.samp_probs[sampled] * len(sampled) / denom, np.ones(batch[0].shape)))
+
+            # Save adjmats
+            adj_out_mats.append(csr_to_torch_coo(
+                csr_adj_mat[batch[i - 1], :][:, batch[i]].multiply(1. / subgraph_probs)).to(
+                self.device))
+
+            # Get the next layer of nodes
+            all_next_nodes = np.unique(csr_adj_mat[batch[i], :].nonzero()[1])
+
+            # Get only the subset of nodes that are 1-hop away
+            new_nodes = np.setdiff1d(all_next_nodes, batch[i])
 
         # Initial layer of the adjacency matrix involves pre-computation,
         # but we already store this, so we only need to slice!
